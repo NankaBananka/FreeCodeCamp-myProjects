13db3a3fd15e7d087359b5fde9f56145bc97c47a
Nelya <nailia.shakirova@gmail.com>
2017-01-09 16:34:52 +0800

    Update 14. Sum All Primes.js

--- Intermediate-Algorithm-Scripting/14. Sum All Primes.js
+++ Intermediate-Algorithm-Scripting/14. Sum All Primes.js
@@ -1,5 +1,12 @@
+/*
+Sum all the prime numbers up to and including the provided number.
+A prime number is defined as a number greater than one and having only two divisors, one and itself. For example, 2 is a prime number because it's only divisible by one and two.
+The provided number may not be a prime.
+*/
+
+
 function sumPrimes(num) {
-  if (num === 1){
+  if (num === 1){  //1 is not a prime
     return 0;
   }
   
@@ -7,7 +14,7 @@ function sumPrimes(num) {
   for (i = 2; i <= num; i++){
     var check = true;
     for (j = 0; j < primes.length; j++){
-      if ((i % primes[j] === 0) && (i !== primes[j])){
+      if ((i % primes[j] === 0) && (i !== primes[j])){  //if we have devider except num itself, then it is not a prime, don't push in array
         check = false;
         break;
       }
@@ -17,6 +24,7 @@ function sumPrimes(num) {
     }
   }
   
+  //reduce method to make a sum of array
   return primes.reduce(function(sum, current){
     return sum + current;
   }, 0);
