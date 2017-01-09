31f21ad0c029001c6105a22f506ded38e7e09c71
Nelya <nailia.shakirova@gmail.com>
2017-01-09 15:18:55 +0800

    Create 14. Smallest Common Multiple.js

---
+++ Intermediate-Algorithm-Scripting/14. Smallest Common Multiple.js
@@ -0,0 +1,32 @@
+function smallestCommons(arr) {
+//sorting array, bcs logically better to begin from largest numbers
+  arr.sort(function(a, b){
+    return b - a;
+  });
+ 
+ //creating ranged array (we need to find least common multiply to all numbers in _range_)
+  var newarr = [];
+  for (i = arr[0]; i >= arr[1]; i--){
+    newarr.push(i);
+  }
+  
+  //Euclid's algorithm for finding greatest common devider (we need it to find LCM)
+  function gcd(x, y){
+    if (x % y === 0){
+      return y;
+    } else{
+      return gcd(y, x % y);
+    }
+}
+  //function for making LCM
+  function scm(x, y){
+    return (x * y)/gcd(x, y);
+  }
+    
+  return newarr.reduce(function(previous, current){
+    var gcdStart = gcd(previous, current);
+    console.log(scm(previous, current));
+    return scm(previous, current);
+  });
+  
+}
